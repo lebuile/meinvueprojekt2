74e0fc7de1e839ade8e970b40e924d4b6a2117f6
“NgogaSandro” <Sandro.Ngoga@Student.HTW-Berlin.de>
2025-06-29 20:08:03 +0200

    MediaFilter fix

--- src/components/MediaFilter.vue
+++ src/components/MediaFilter.vue
@@ -1,11 +1,29 @@
+<script setup lang="ts">
+import { defineEmits } from 'vue'
+
+const emit = defineEmits<{
+  'filter-change': [filter: string]
+}>()
+
+const handleFilterChange = (event: Event) => {
+  const target = event.target as HTMLSelectElement
+  emit('filter-change', target.value)
+}
+</script>
+
 <template>
-  <select @change="$emit('filter-change', $event.target.value)">
-    <option value="">Alle</option>
-    <option value="MOVIE">Film</option>
-    <option value="SERIES">Serie</option>
-  </select>
+  <div>
+    <select @change="handleFilterChange">
+      <option value="">Alle</option>
+      <option value="movie">Filme</option>
+      <option value="series">Serien</option>
+    </select>
+  </div>
 </template>
 
-<script setup lang="ts">
-defineEmits(['filter-change'])
-</script>
+<style scoped>
+select {
+  padding: 0.5rem;
+  margin: 0.5rem;
+}
+</style>
--- tests/unit/MediaFilter.spec.ts
+++ tests/unit/MediaFilter.spec.ts
@@ -3,12 +3,12 @@ import { mount } from '@vue/test-utils'
 import MediaFilter from '@/components/MediaFilter.vue'
 
 describe('MediaFilter', () => {
-  it('emittiert filter-change Event bei Typ-Änderung', async () => {
+  it("emittiert filter-change Event bei Typ-Änderung", async () => {
     const wrapper = mount(MediaFilter)
     const select = wrapper.find('select')
-    await select.setValue('MOVIE')
+    await select.setValue('movie')  // 'movie' statt 'MOVIE'
     expect(wrapper.emitted('filter-change')).toBeTruthy()
-    expect(wrapper.emitted('filter-change')?.[0]).toEqual(['MOVIE'])
+    expect(wrapper.emitted('filter-change')?.[0]).toEqual(['movie'])  // 'movie' statt 'MOVIE'
   })
 
   it('zeigt alle Filter-Optionen an', () => {
